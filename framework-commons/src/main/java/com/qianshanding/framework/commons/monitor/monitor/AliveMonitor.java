8a67a01f6e275c49536aa9da68553e1822d71245
前山顶 <qianshanding@163.com>
2017-06-13 12:08:37 +0800

    gzip helper

---
+++ framework-commons/src/main/java/com/qianshanding/framework/commons/monitor/monitor/AliveMonitor.java
@@ -0,0 +1,20 @@
+package com.qianshanding.framework.commons.monitor.monitor;
+
+/**
+ * Created by zhengyu on 2016/11/21.
+ */
+class AliveMonitor implements Monitor {
+
+    final static AliveMonitor instance = new AliveMonitor();
+
+    public static AliveMonitor getInstance() {
+        return instance;
+    }
+
+    private AliveMonitor() {
+    }
+
+    @Override
+    public void run() {
+    }
+}
